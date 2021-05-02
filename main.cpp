54dcf0d5364d04fa138c5282510155fde4f471e1
Gömböcz Zsolt <zsolt.gombocz00gmail.com>
2021-05-02 13:03:32 +0200

    Tested the constructors and virtual methods

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+#include <Plain.h>
+#include <Lake.h>
+#include <Green.h>
+
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    Lake l("Bean", 86.0);
+    Plain p("Dean", 12.0);
+    Green g("Green", 26.0);
+
+    cout << l.get_type() << p.get_type() << g.get_type();
     return 0;
 }
