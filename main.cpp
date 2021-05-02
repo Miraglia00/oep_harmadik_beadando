ed2c58928d59bd85d61115b85b6ae90c12f3a53e
Gömböcz Zsolt <zsolt.gombocz00gmail.com>
2021-05-02 15:19:28 +0200

    Testing

--- main.cpp
+++ main.cpp
@@ -11,7 +11,12 @@ int main()
     Lake l("Bean", 86.0);
     Plain p("Dean", 12.0);
     Green g("Green", 26.0);
+    Plain p1("Pean", 17.0);
 
-    cout << l.get_type() << p.get_type() << g.get_type();
+    l.print();
+    p.print();
+    p.set_humidity(80);
+    g.print();
+    p1.print();
     return 0;
 }
