8ba0132e90835e5c1d478fbc2018ecdd54fe0e89
Gömböcz Zsolt <zsolt.gombocz00gmail.com>
2021-05-02 22:49:58 +0200

    Earth initialized and listed all lands in the start

--- main.cpp
+++ main.cpp
@@ -1,22 +1,11 @@
 #include <iostream>
-#include <Plain.h>
-#include <Lake.h>
-#include <Green.h>
-
+#include <Earth.h>
 
 using namespace std;
 
 int main()
 {
-    Lake l("Bean", 86.0);
-    Plain p("Dean", 12.0);
-    Green g("Green", 26.0);
-    Plain p1("Pean", 17.0);
-
-    l.print();
-    p.print();
-    p.set_humidity(80);
-    g.print();
-    p1.print();
+    Earth e("input.txt");
+    e.listLands();
     return 0;
 }
